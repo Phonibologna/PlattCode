6739adfedb10ecf0c3b1e76627d84c61a044c8fe
Phonibologna <skennedsl@gmail.com>
2020-01-15 10:15:02 -0800

    Add project files.

---
+++ InventoryProject/Controllers/ItemController.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using InventoryProject.Models;
+using Microsoft.AspNetCore.Mvc;
+using MySql.Data.MySqlClient;
+
+namespace InventoryProject.Controllers
+{
+    public class ItemController : Controller
+    {
+        string connStr = "server=automap.cxnllsgrt5yp.us-west-2.rds.amazonaws.com;uid=scott;pwd=kennedy;database=ScottsDB";
+        public IActionResult Index()
+        {
+            List<Item> itemList = new List<Item>();
+            string sqlStr = "";
+            MySqlConnection connection = null;
+            MySqlCommand cmd = null;
+            try {
+                //open a connection and check for any existing data on the database.
+                connection = new MySqlConnection(connStr);
+                connection.Open();
+                sqlStr = "SELECT * FROM Items";
+                cmd = new MySqlCommand(sqlStr, connection);
+                MySqlDataReader reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    //make new items to populate the table.
+                    string iName = reader["itemName"].ToString();
+                    string iNum = reader["itemNum"].ToString();
+                    string iManufacturer = reader["Manufacturer"].ToString();
+                    string iDescription = reader["Description"].ToString();
+                    string iCategory = reader["Category"].ToString();
+                    double iPrice = (double)reader["Price"];
+
+                    Item AddItem = new Item(iName, iNum, iManufacturer, iDescription, iCategory, iPrice);
+                    itemList.Add(AddItem);
+                }
+
+
+            }catch(Exception exc)
+            {
+                Console.WriteLine(exc);
+            }
+            if (null != connection)
+                connection.Close();
+                //closes connection once data is retrieved, if it's open.
+
+            return View(itemList);
+        }
+
+        public IActionResult Create()
+        {
+            //create windows start here, so they can be blank.
+            return View();
+        }
+        public IActionResult Remove(Item item)
+        {
+            //remove starts here so we can have a nice confirmation window.
+            return View(item);
+        }
+
+        [HttpPost]
+        public IActionResult Remove(string itemNum)
+        {
+            string sqlStr = "";
+            MySqlConnection connection = null;
+            MySqlCommand cmd = null;
+            try
+            {
+                connection = new MySqlConnection(connStr);
+                connection.Open();
+                sqlStr = "DELETE FROM Items WHERE itemNum = '" + itemNum + "'";
+                cmd = new MySqlCommand(sqlStr, connection);
+                cmd.ExecuteNonQuery();
+
+            }
+            catch (Exception exc)
+            {
+                Console.WriteLine(exc);
+
+            }
+            //close connection
+            if (null != connection)
+                connection.Close();
+            return RedirectToAction("Index");
+        }
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Edit(Item item)
+        {
+            return View(item);
+        }
+        [HttpPost]
+        public IActionResult Edit(string oldNumber, string itemName, string itemNum, string Manufacturer, string Description, string Category, double Price)
+        {//oldNumber exists in case we end up editing the itemNumber so we can still find it in the database.
+            if (ModelState.IsValid)
+            {
+                //attempt to connect to sql server.
+
+                string sqlStr = "";
+                MySqlConnection connection = null;
+                MySqlCommand cmd = null;
+                try
+                {
+                    connection = new MySqlConnection(connStr);
+                    connection.Open();
+                    sqlStr = "UPDATE Items SET itemNum = '" + itemNum + "', itemName = '" + itemName + "', Manufacturer = '" + Manufacturer + "', Description ='" + Description
+                        + "', Category = '" + Category + "', Price = '" + Price + "' WHERE itemNum = '" + oldNumber + "'";
+                    cmd = new MySqlCommand(sqlStr, connection);
+                    cmd.ExecuteNonQuery();
+
+                }
+                catch (Exception exc)
+                {
+                    Console.WriteLine(exc);
+
+                }
+                //close connection
+                if (null != connection)
+                    connection.Close();
+                return RedirectToAction("Index");
+            }
+            Item item = new Item(itemName, itemNum, Manufacturer, Description, Category, Price);
+            return View(item);
+        }
+        
+        
+        
+
+        [HttpPost]
+        public IActionResult Create(Item item)
+        {//this does the actual heavy lifting for the creation.
+            if(ModelState.IsValid)
+            {
+                //attempt to connect to sql server.
+
+                string sqlStr = "";
+                MySqlConnection connection = null;
+                MySqlCommand cmd = null;
+                try
+                {
+                    connection = new MySqlConnection(connStr);
+                    connection.Open();
+                    sqlStr = "INSERT INTO Items (itemNum, itemName, Manufacturer, Description, Category, Price) VALUES ('" + item.itemNum + "', '" + item.itemName + "', '" +
+                    item.Manufacturer + "', '" + item.Description + "', '" + item.Category + "', '" + item.Price + "')";
+                    cmd = new MySqlCommand(sqlStr, connection);
+                    cmd.ExecuteNonQuery();
+
+                }
+                catch (Exception exc)
+                {
+                    Console.WriteLine(exc);
+                    
+                }
+                //close connection
+                if (null != connection)
+                    connection.Close();
+                return RedirectToAction("Index");
+            }
+            return View(item);
+        }
+
+
+    }
+}
---
+++ InventoryProject/Models/ItemModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InventoryProject.Models
+{
+    public class Item
+    {
+        //begin by declaring properties and variables
+
+        [Display(Name = "Item Name")] [Required] public string itemName { get; set; }
+        [Display(Name = "Item Number")] [Required] public string itemNum { get; set; }
+        public string Manufacturer { get; set; }
+        public string Description { get; set; }
+        [Required] public string Category { get; set; }
+
+        [DataType(DataType.Currency)] [Range(0,500)] public double Price { get; set; }
+
+        //constructors.
+        public Item(string itemName, string itemNum, string manufacturer, string description, string category, double price)
+        {
+            this.itemName = itemName;
+            this.itemNum = itemNum;
+            this.Manufacturer = manufacturer;
+            this.Description = description;
+            this.Category = category;
+            this.Price = price;
+        }
+
+        public Item()
+        {
+            this.itemName = "";
+            this.itemNum = "0";
+            this.Manufacturer = "";
+            this.Description = "";
+            this.Category = "";
+            this.Price = 0.0;
+        }
+
+
+    }
+}
